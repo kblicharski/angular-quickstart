c59dc7ad578c37ca7c34b9a78f1f79f4cab6432c
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-11-02 06:21:20 -0500

    Include a routing module and about page.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './utility/page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: '/about', pathMatch: 'full'},
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/404'},
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './utility/page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
