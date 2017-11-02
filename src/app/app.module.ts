0eb18f5a773323e4024d384f94e5797fc6e5cdf4
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-11-02 07:23:35 -0500

    Reformat files, expand about page with links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app-material.module';
+
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './utility/page-not-found/page-not-found.component';
 import { AboutComponent } from './about/about.component';
-import { AppRoutingModule } from './app-routing.module';
-
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
     AboutComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
